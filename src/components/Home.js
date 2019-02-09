f45e677f812d7dc65810ad2e64d0cd9ca7aa339f
ina_tedy@hotmail.com <ina_tedy@hotmail.com>
2019-02-09 17:07:08 +0100

    me falto la imagen de bienvenida

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,8 @@ const Home = () => {
     return(
         <div className="components-container">
             <h3>Las movies</h3>
+            <img src="https://pm1.narvii.com/6535/58f2e507d668c9c9f37f8e377c9429073c309391_00.jpg" alt="Bienvenidos" />
+            
         </div>
     )
 }
